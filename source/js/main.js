776c3178d84e001755054164ce826e1f2527b3fb
Morozova Tatiana <tavimo14@gmail.com>
2025-03-13 00:11:31 +0500

    Добавляет разметку и js вопросы

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,14 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+
+const items = document.querySelectorAll('.faq__item'); 
+
+items.forEach(item => {
+  const toggleButton = item.querySelector('.faq__item-button'); 
+
+  toggleButton.addEventListener('click', () => {
+    item.classList.toggle('faq__item--open'); 
+  });
+});
