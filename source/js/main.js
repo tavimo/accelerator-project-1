e95ef54bc633962f44404b80239602216e58514d
Morozova Tatiana <tavimo14@gmail.com>
2025-03-17 20:08:34 +0500

    Исправляет код по линтерам

--- source/js/main.js
+++ source/js/main.js
@@ -4,28 +4,27 @@
 // import 'swiper/css';
 
 
-
 //
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   // Переключение вкладок
   const tabs = document.querySelectorAll('.faq__sorting-item');
   const lists = document.querySelectorAll('.faq__list');
 
   // Сохраняем состояние вкладок
-  let activeTab = localStorage.getItem('activeTab') || 'центр';
+  const activeTab = localStorage.getItem('activeTab') || 'центр';
 
   // Функция для отображения активной вкладки
   function setActiveTab(tab) {
-    tabs.forEach(tabItem => {
+    tabs.forEach((tabItem) => {
       tabItem.classList.remove('faq__sorting-item--current'); // Убираем класс с всех вкладок
     });
-    const activeTabElement = [...tabs].find(tabItem => tabItem.querySelector('button').textContent.toLowerCase() === tab);
+    const activeTabElement = [...tabs].find((tabItem) => tabItem.querySelector('button').textContent.toLowerCase() === tab);
     if (activeTabElement) {
       activeTabElement.classList.add('faq__sorting-item--current'); // Добавляем класс активной вкладке
     }
 
-    lists.forEach(list => {
+    lists.forEach((list) => {
       list.style.display = 'none'; // Прячем все списки
     });
     const activeList = document.querySelector(`.faq__list[data-tab="${tab}"]`);
@@ -37,8 +36,8 @@ document.addEventListener('DOMContentLoaded', function () {
   // Инициализация вкладки, основываясь на сохраненном состоянии
   setActiveTab(activeTab);
 
-  tabs.forEach(tab => {
-    tab.addEventListener('click', function () {
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
       const tabName = tab.querySelector('button').textContent.toLowerCase();
       setActiveTab(tabName);
       localStorage.setItem('activeTab', tabName); // Сохраняем активную вкладку в локальное хранилище
@@ -46,13 +45,13 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   // Работа с аккордеонами
-  const items = document.querySelectorAll('.faq__item'); 
+  const items = document.querySelectorAll('.faq__item');
 
-items.forEach(item => {
-  const toggleButton = item.querySelector('.faq__item-button'); 
+  items.forEach((item) => {
+    const toggleButton = item.querySelector('.faq__item-button');
 
-  toggleButton.addEventListener('click', () => {
-    item.classList.toggle('faq__item--open'); 
+    toggleButton.addEventListener('click', () => {
+      item.classList.toggle('faq__item--open');
+    });
   });
 });
-  });
